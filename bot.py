1d2bc370ccf8a0494ec83c1e15d0db7797c2463b
Nick Jackson <nick@nickjackson.me>
2016-06-07 13:55:33 +0100

    Let's play with Flask!

--- bot.py
+++ bot.py
@@ -2,12 +2,24 @@
 # encoding: utf-8
 
 import os
-from bottle import run, post
+from flask import Flask
+from flask_slack import Slack
+
+app = Flask(__name__)
+
+slack = Slack(app)
+app.add_url_rule('/', view_func=slack.dispatch)
 
 
-@post('/hello')
-def hello():
-    return 'Hello World!'
+# Hi world!
+@slack.command('hello', token=os.environ.get("SLACK_TOKEN"),
+               team_id=os.environ.get("SLACK_TEAM_ID"), methods=['POST'])
+def hello(**kwargs):
+    text = kwargs.get('text')
+    return slack.response(text)
 
-if __name__ == '__main__':
-    run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
+
+# Fire it up!
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
