b82d76b6cbb0aa5ef4e137d81faa51f1c66bab23
Nick Jackson <nick@nickjackson.me>
2016-06-07 14:25:25 +0100

    Lunch! Om nom!

--- bot.py
+++ bot.py
@@ -4,19 +4,45 @@
 import os
 from flask import Flask
 from flask_slack import Slack
+import random
+import urllib
+import json
 
+
+# Let's get a Flask app going
 app = Flask(__name__)
-
 slack = Slack(app)
 app.add_url_rule('/', view_func=slack.dispatch)
 
 
+# Configuration!
+# Where can we find the menu?
+menu_url = 'https://raw.githubusercontent.com/duke-studios/cafeteria-data/master/opening-hours.json'
+
+
 # Hi world!
-@slack.command('hello', token=os.environ.get("SLACK_TOKEN"),
-               team_id=os.environ.get("SLACK_TEAM_ID"), methods=['POST'])
-def hello(**kwargs):
-    text = kwargs.get('text')
-    return slack.response(text)
+@slack.command('lunch', token=os.environ.get("SLACK_TOKEN"), methods=['POST'])
+def lunch(**kwargs):
+
+    prefixes = [
+        "I think you should try the",
+        "You want to have the",
+        "Why not try the",
+        "I recommend the",
+        "You want the",
+        "My suggestion is the"
+    ]
+
+    response = urllib.urlopen(menu_url)
+    data = json.loads(response.read())
+
+    item = random.choice(data['lunch']['items'])
+    prefix = random.choice(prefixes)
+    text = "%s %s (%s)." % (prefix, item['name'], item['price'])
+
+    return slack.response(
+        text=text,
+        response_type='in_channel')
 
 
 # Fire it up!
