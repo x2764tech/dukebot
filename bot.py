a9b55324300a537268b5c37c156995308e55f9c2
Nick Jackson <nick@nickjackson.me>
2016-06-07 13:46:01 +0100

    I hate Python

--- bot.py
+++ bot.py
@@ -10,4 +10,4 @@ def hello():
     return 'Hello World!'
 
 if __name__ == '__main__':
-    run(host='0.0.0.0', port=os.environ['PORT', 5000])
+    run(host='0.0.0.0', port=os.environ('PORT', 5000))
