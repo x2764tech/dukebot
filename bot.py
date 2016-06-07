a17a4b35bb05b4e9303dd26ac98de7cf5e106bdc
Nick Jackson <nick@nickjackson.me>
2016-06-07 13:44:46 +0100

    Use the port that Heroku prefers

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import os
 from bottle import run, post
 
 
@@ -9,4 +10,4 @@ def hello():
     return 'Hello World!'
 
 if __name__ == '__main__':
-    run(host='0.0.0.0')
+    run(host='0.0.0.0', port=os.environ['PORT', 5000])
